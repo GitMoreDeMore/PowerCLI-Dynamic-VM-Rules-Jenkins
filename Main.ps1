cf0ad204312eb75ea7fc93c13c7843d6af9d6deb
MikeD <MikeD@outlook.com>
2022-02-01 22:17:00 -0600

    Update Tags and Match_VM variables

--- Main.ps1
+++ Main.ps1
@@ -1,12 +1,16 @@
-##Create DRS rules dynamically for matching Client_Code/Workload_Type Tags
-
-# Connect to vCenter Server
-Connect-VIServer -Server 192.168.1.159 -WarningAction SilentlyContinue
+## Create DRS rules dynamically for matching Client_Code/Workload_Type Tags
 
 # Import Function Modules
-Import-Module -Name .\Modules\Get-DRSVMRules.ps1
-Import-Module -Name .\Modules\Get-ClientCodes.ps1
-Import-Module -Name .\Modules\Add-DRSVMRules.ps1
+. .\Modules\Get-DRSVMRules.ps1
+. .\Modules\Get-ClientCodes.ps1
+. .\Modules\Add-DRSVMRules.ps1
+
+$vCenter = ""
+$User = ""
+$Password = ""
+
+# Connect to vCenter Server
+Connect-VIServer -Server $vCenter -User $User -Password $Password -WarningAction SilentlyContinue
 
 # Gather Workload Tags from vCenter IE. RGI,WEB,MDB
 $Server_Options = Get-Tag -Category "Workload_Type" | Where-Object {$_.name -notmatch "MOD|TRASH|GARBAGE"}
--- Modules/Add-DRSVMRules.ps1
+++ Modules/Add-DRSVMRules.ps1
@@ -3,13 +3,13 @@ function Add-DRSVMRules
     Param([string] $Client_Code,[string] $Cluster,[string[]] $Server_Options)
     foreach($Server_Type in $Server_Options)
         {
-        $Match_VMs = Get-VM -Tag $Client_Code | Get-TagAssignment | Where-Object { $_.Tag -like "$Server_Type"}
+        $Match_VMs = Get-VM -Tag $Client_Code | Get-TagAssignment | Where-Object { $_.Tag -like "*$Server_Type*"}
         if ($Match_VMs.count -gt 1)
             {
             Write-Host "$Client_Code"
-            Write-Host "$Match_VMs"
+            Write-Host "$Match_VMs.Entity.Name"
             Write-Host "$Cluster"
-            New-DrsRule -Cluster $Cluster -Name AA"_"$Client_Code"_"$Server_Type -KeepTogether $false -VM $Match_VMs
+            New-DrsRule -Cluster $Cluster -Name AA"_"$Client_Code"_"$Server_Type -KeepTogether $false -VM $Match_VMs.Entity.Name
             }
         }
     }
